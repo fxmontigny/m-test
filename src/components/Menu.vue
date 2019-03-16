4d29bdbab91692b8a1c6f2ae16c56da798931dbb
François-Xavier Montigny <fxaviermontigny@gmail.com>
2019-03-16 09:48:12 +0100

    [Add] load cat by breed id

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,6 +2,7 @@
   <div class="main">
     <div class="wrapper">
       <p>The cat website</p>
+      <b-form-select v-model="breedSelected" :options="breedsOptions"/>
     </div>
   </div>
 </template>
@@ -13,15 +14,27 @@ import store from "@/stores/CatStore";
 export default {
   data() {
     return {
-      breeds: store.state.breeds
+      breeds: store.state.breeds,
+      breedsOptions: [],
+      breedSelected: null
     };
   },
+  watch: {
+    breedSelected(newBreed, olBreed) {
+      console.log("on change breed", newBreed, olBreed);
+
+      this.loadCats(newBreed);
+    }
+  },
   methods: {
-    loadCats(breed) {
+    loadCats(breedId) {
       axios
-        .get("https://api.thecatapi.com/v1/images/search", {
-          params: { limit: 10, size: "full", breed }
-        })
+        .get(
+          "https://api.thecatapi.com/v1/images/search?breed_ids=" + breedId,
+          {
+            params: { limit: 10, size: "full" }
+          }
+        )
         .then(response => {
           console.log("-- Image from TheCatAPI.com");
           console.log(response.data);
@@ -32,6 +45,16 @@ export default {
     // redirect to home page if no breeds
     if (this.breeds === null) {
       this.$router.replace("/");
+    } else {
+      // prepare list with value and text
+      this.breedsOptions = [];
+      this.breeds.map((b, index) => {
+        this.breedsOptions.push({ value: b.id, text: b.name });
+
+        if (index === 0) {
+          this.breedSelected = b.id;
+        }
+      });
     }
   }
 };
@@ -45,8 +68,18 @@ export default {
   color: white;
 }
 
+.wrapper {
+  display: flex;
+  align-items: center;
+}
+
 .wrapper p {
   padding: 0 45px;
   margin: 0;
+  flex: 1;
+}
+
+select {
+  width: 200px;
 }
 </style>
